8b8fdf1acdb82a2123959f15765553b65aad954d
Mhenryn Cabanit <mhenryn.cabanit@gmail.com>
2019-10-11 16:07:00 +0800

    jQuery game update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,101 +1,143 @@
-
 // player object
-const player = {
+let player = {
 	character : $("#player"),
 	playerPos : 0
 }
 
-console.log(player)
-
 // ai object
 const ai = {
-	character : $("#ai"),
-	aiPos : 0
+	character: $("#ai"),
+	aiPos: 0
 }
 
-console.log(ai)
 
 let playerPosition = 0;
 let aiPosition = 0;
 
-// move the player function
-player.character.click(function() {
-	console.log($(this))
-
-	$(this).animate({
-		'left' :  (playerPosition+=100) + 'px'
-	})
-
-	console.log('this is the current value of player position: ' + playerPosition)
-	player.playerPos = playerPosition
-
-	console.log(player)
-
-	result()
-})
 
 const aiMove = (speed) => {
 	if(ai.aiPos < 1000) {
 		ai.character.animate({
-			'left' : (aiPosition+=speed) + "px"
+		'left' : (aiPosition += 50) + 'px',
 		})
 	} else {
 		return true
 	}
-	console.log(ai.aiPos)
+	console.log("Ai Position: " + ai.aiPos)
 	ai.aiPos = aiPosition
+	// setTimeout(aiMove, 500)
 	result()
-	setTimeout(()=> aiMove(speed), 500)
+	if(ai.aiPos >= 1000 || player.playerPos >= 1000) {
+		alert("finish line na! wag pagala-gala!")
+		return true
+	}
+	else {
+		setTimeout(()=> aiMove(speed), 500)
+	}
 }
 
-// move the ai
-$("#start").click(function() {
-	$("#start").removeClass('d-block')
-	$("#start").addClass('d-none')
-	aiMove()
-})
+
+const playerMove = (speed) => {
+	if(player.playerPos < 1000) {
+		player.character.animate({
+			'left' : (playerPosition += 50) + 'px',
+		})
+	} else {
+		return true
+	}
+	console.log("Player Position: " + player.playerPos)
+	player.playerPos = playerPosition
+	// setTimeout(playerMove, 500)
+	result()
+	if(ai.aiPos >= 1000 || player.playerPos >= 1000) {
+		alert("finish line na! wag pagala-gala!")
+		return true
+	}
+	else {
+		setTimeout(()=> playerMove(speed), 500)
+	}
+}
 
 const result = () => {
-	if(player.playerPos == 1000) {
-		alert('player won!')
+	if(ai.aiPos == 1000) {
+		alert('ghost # 1 won!')
+		player.character.animate({
+			'left' : 1000 + 'px',
+		})
 		return true
 	}
-
-	if(ai.aiPos == 1000) {
-		alert("ai won!")
+	if(player.playerPos == 1000) {
+		alert('ghost # 2 won!')
+		ai.character.animate({
+		'left' : 1000 + 'px',
+	})
 		return true
 	}
 }
+
+
+
+
 
 
 $(".btn").click(function() {
-	// console.log($(this).attr('id') == '1')
+	
 
-	// if($(this).attr('id') == 1) {
+	// if($(this).attr('#easy') ) {
 	// 	aiMove(20)
+	// 	playerMove(20)
 	// }
 
-	// if($(this).attr('id') == 2) {
+	// if($(this).attr('#medium') ) {
 	// 	aiMove(30)
+	// 	playerMove(30)
 	// }
 
-	// if($(this).attr('id') == 3) {
+	// if($(this).attr('#hard')) {
 	// 	aiMove(80)
+	// 	playerMove(80)
 	// }
 
-})
-
-$('body').keydown(function(e) {
-	console.log(e.which)
-	if(e.which == 40) {
-		alert("hello")
-	}
+	aiMove()
+	playerMove()
 })
 
 
 
+$("#fast1").click(function() {
+	// aiMove()
+	// playerMove()
+		player.character.animate({
+			'left' : (playerPosition += 20) + 'px',
+		})
+})
 
+$("#fast2").click(function() {
+	// aiMove()
+	// playerMove()
+	ai.character.animate({
+		'left' : (aiPosition += 20) + 'px',
+	})
+})
 
+$("#try").click(function() {
+	// aiMove()
+	// playerMove()
+	let aiPos = 0;
+	let playerPos = 0;
+	ai.character.animate({
+		'left' : 0 + 'px',
+	})
 
+	player.character.animate({
+		'left' : 0 + 'px',
+	})
 
+	ai.aiPos = 0;
+	player.playerPos = 0;
 
+	$(".second").click(); 
+    return false;
+
+	
+})
